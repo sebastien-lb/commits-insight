cde1d17ac0939e431bf829262f865cfbed0a7c80
sebastien-lb <sebast.lubineau@laposte.net>
2019-09-25 12:24:54 +0200

    feat: fetch 150 previous commits from repo

--- src/redux/github/github.api.ts
+++ src/redux/github/github.api.ts
@@ -1,12 +1,23 @@
 import axios from "axios";
+import { getNextCommitsLink } from "../../services/github-link";
+
+const MAX_PAGE_RETRIEVE = 5;
 
 export const makeFetchCommitsCall = async (
   accessToken: string | undefined,
   repoName: string | undefined,
   owner: string | undefined
 ) => {
-  const rep = await axios.get(
+  let rep = await axios.get(
     `https://api.github.com/repos/${owner}/${repoName}/commits?access_token=${accessToken}`
   );
-  return rep.data;
+  let nbPageRetrieved = 1;
+  let data = rep.data;
+  while (rep.headers.link && nbPageRetrieved < MAX_PAGE_RETRIEVE) {
+    const nextLink = getNextCommitsLink(rep.headers.link);
+    nbPageRetrieved += 1;
+    rep = await axios.get(nextLink);
+    data = [...data, ...rep.data];
+  }
+  return data;
 };
