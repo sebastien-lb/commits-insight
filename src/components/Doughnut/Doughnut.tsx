f60f0ef5f193e85bbbe6bea4fb52cf8632b88a05
sebastien-lb <sebast.lubineau@laposte.net>
2019-09-26 14:23:35 +0200

    refactor: use functional component instead of class

--- src/components/Doughnut/Doughnut.tsx
+++ src/components/Doughnut/Doughnut.tsx
@@ -13,16 +13,14 @@ interface OwnProps {
 
 type Props = OwnProps;
 
-export class DoughnutComponent extends React.Component<Props> {
-  public render(): JSX.Element {
-    const { chartData, classes } = this.props;
-    return (
-      <div className={classes.container}>
-        <Doughnut width={300} height={300} data={chartData} />
-      </div>
-    );
-  }
-}
+export const DoughnutComponent: React.FC<Props> = (props: Props) => {
+  const { chartData, classes } = props;
+  return (
+    <div className={classes.container}>
+      <Doughnut width={300} height={300} data={chartData} />
+    </div>
+  );
+};
 
 const styles: Style = (theme: Theme): Record<ClassNames, CSSProperties> => ({
   container: {}
